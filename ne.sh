bf4bbfb34a1a70ca5b9d54cbe0a9d38d40105fb6
CrXane <25283652+CrXane@users.noreply.github.com>
2020-04-26 05:18:35 -0400

    Create ne.sh

---
+++ ne.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while IFS= read -r l; do
+ if [[ $l =~ ^[^a-zA-Z] ]]; then
+  loc=$(geoiplookup $l)
+  loc=${loc:23:2}
+  if [ $loc != "IP" ]; then
+   echo "[%loc] %l"
+  fi
+ fi
+done< <(netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq)
