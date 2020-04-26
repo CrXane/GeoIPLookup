618c3aa8d4080d88308f6ca8c949ce75741bc7c0
CrXane <25283652+CrXane@users.noreply.github.com>
2020-04-26 11:04:05 -0400

    Update ne.sh

--- ne.sh
+++ ne.sh
@@ -5,7 +5,7 @@ while IFS= read -r l; do
   loc=$(geoiplookup $l)
   loc=${loc:23:2}
   if [ $loc != "IP" ]; then
-   echo "[%loc] %l"
+   echo "[$loc] $l"
   fi
  fi
 done< <(netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq)
